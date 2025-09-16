9301ede7242aa4d9867ce5fcfda5a413caa30922
Adre <dreenation21@gmail.com>
2025-09-16 10:53:18 +0700

    feat: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
+use App\Models\Order;
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -28,6 +30,23 @@ class DatabaseSeeder extends Seeder
             'name' => 'Mulya Jaya Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('mulyajaya'),
+        ]);
+
+        User::create([
+            'name' => 'Adrenalin',
+            'email' => 'adrenalin@gmail.com',
+            'password' => Hash::make('adrenalin'),
+            'phone_number' => '088222134661',
+            'address' => 'Jl. Maleber Barat',
+        ]);
+
+        Order::create([
+            'user_id' => 1,
+            'product_id' => 1,
+            'address' => 'Jl. Maleber Barat',
+            'order_date' => Carbon::now(),
+            'total_price' => 200000,
+            'status' => 'pending',
         ]);
     }
 }
